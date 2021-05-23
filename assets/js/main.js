269f9d54d2b1be979c5009e7d1a21a36b8f2ca1a
bluninjadragon <jwhbootcamp@gmail.com>
2021-05-23 18:27:01 -0500

    adding the beginnings of the 5 day forecast function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
-const city = document.querySelector("#city-input");
+const cityInput = document.querySelector("#city-input");
 const button = document.querySelector("#city-button");
 const mainCity = document.querySelector("#main-city");
 const mainTemp = document.querySelector("#main-temp");
 const mainWind = document.querySelector("#main-wind");
 const mainHum = document.querySelector("#main-hum");
 const mainUVI = document.querySelector("#main-uvi");
+const oneTemp = document.querySelector("#one-temp");
 
 function fetchCoord() {
   let requestOptions = {
@@ -12,7 +13,7 @@ function fetchCoord() {
     redirect: 'follow'
   };
   
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=imperial&appid=1e9fe6b8a2be8c30b4ddc5a45ef6c932`, requestOptions)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&units=imperial&appid=1e9fe6b8a2be8c30b4ddc5a45ef6c932`, requestOptions)
     .then(response => response.text())
     .then(result => {
       const data = JSON.parse(result);
@@ -37,14 +38,29 @@ function fetchWeather(lat, lon) {
     .then(result => {
       const weather = JSON.parse(result);
       console.log(weather)
-      mainCity.textContent = city.value;
+      mainCity.textContent = cityInput.value;
       mainTemp.textContent = `Temp: ${weather.current.temp} °F`;
       mainWind.textContent = `Wind: ${weather.current.wind_speed} mph`;
       mainHum.textContent = `Humidity: ${weather.current.humidity} %`;
       mainUVI.textContent = `UV Index: ${weather.current.uvi}`;
+
+      fdForecast(weather);
     })
     .catch(error => console.log('error', error));
 }
 
+function fdForecast(weather) {
+  console.log(weather.daily[1].temp.day);
+  console.log(weather.daily[1].wind_speed);
+  console.log(weather.daily[1].humidity);
+
+  oneTemp.textContent = `Temp: ${weather.current.temp} °F`;
+  
+}
+
+
 //event listeners for button
 button.addEventListener("click", fetchCoord);
+// // code for enter from StackOverflow
+// cityInput.addEventListener('keypress', function (e) {
+//   if (e.key === 'Enter') {}}, fetchCoord);
