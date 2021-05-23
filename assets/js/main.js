c7807418bd70683f5e9cbd957a92550cd44b07a0
bluninjadragon <jwhbootcamp@gmail.com>
2021-05-23 03:55:08 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,19 @@
-function init() {
-    getApi();
+const city = document.querySelector("#city");
+const button = document.querySelector("#city-button");
+
+
+function fetch() {
+  let requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("api.openweathermap.org/data/2.5/weather?q=Chicago&appid=1e9fe6b8a2be8c30b4ddc5a45ef6c932", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 }
 
-function getApi() {
-    // fetch request gets a list of all the repos for the node.js organization
-    const requestUrl = 'https://openweathermap.org/api/one-call-api';
-  
-    fetch(requestUrl)
-      .then((response) => response.text())
-      .then((result) => {
-        console.log(result)
-      })
-    }
-init();
+//event listeners for button
+// button.forEach((btn) => btn.addEventListener("click", fetch));
+fetch();
